0f6b611d2d6d3542c9af9c618e0c41512a0e44dc
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2021-12-06 20:55:39 -0500

    fix(Parser): Updated parameter attributes for `Add-OrgElement`
    * Added Position 0 to the Child  parameter
    
    * The Parent parameter looks on the pipeline first, then the -Parent
      param, so:
      ```powershell
      # either call it like this:
      $root | Add-Element $headline
      # -or-
      Add-Element $headline -Parent $root
      ```
    
      Don't do this:
      ```powershell
      Add-Element $root $headline
      ```
    
    * Updated the comment-based help

--- tests/Element/Add-OrgElement.Tests.ps1
+++ tests/Element/Add-OrgElement.Tests.ps1
@@ -1,8 +1,23 @@
 
-Describe "Testing private Element function Add-OrgElement" -Tags @('unit', 'OrgElement', 'Add' ) {
-    Context "Basic functionality" {
-        It "Should load without error" {
-            {Get-Help Add-OrgElement -ErrorAction Stop} | Should -Not -Throw
+InModuleScope -ModuleName 'PSOrgMode' {
+    Describe 'Testing private Element function Add-OrgElement' -Tags @('unit', 'OrgElement', 'Add' ) {
+        Context 'Basic functionality' {
+            It 'Should load without error' {
+                { Get-Help Add-OrgElement -ErrorAction Stop } | Should -Not -Throw
+            }
+        }
+        Context 'When Parent is passed on the pipeline and Child(ren) are Args' {
+            BeforeAll {
+                $root = New-OrgElement -Type orgdata
+                $h = New-OrgElement -Type headline
+                $h.level = 1
+                $h.title = 'a test heading'
+            }
+            It 'Should add the arguments to the pipeline element' {
+                $root | Add-OrgElement $h
+                $root.Children[0].level | Should -Be 1
+                $root.Children[0].title | Should -Be 'a test heading'
+            }
         }
     }
 }
