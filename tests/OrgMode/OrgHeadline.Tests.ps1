26d7f6dcbb27ebe492448a27346f5aebf1394b5b
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-03-08 15:29:50 -0500

    task: Migrate class tests from in-module to dotsource sources

--- tests/OrgMode/OrgHeadline.Tests.ps1
+++ tests/OrgMode/OrgHeadline.Tests.ps1
@@ -1,10 +1,42 @@
 
-InModuleScope -ModuleName "PSOrgMode" {
-    Describe "Testing OrgMode class OrgHeadline" -Tags @('unit', 'OrgHeadline') {
-        Context "Class can be loaded" {
-            It "Does not throw an error when instantiated" {
-                {$null = New-Object OrgHeadline} | Should -Not -Throw
-            }
+BeforeDiscovery {
+    . "$($Source.Path)\OrgMode\enum\OrgType.ps1"
+    . "$($Source.Path)\OrgMode\enum\OrgRepeatType.ps1"
+    . "$($Source.Path)\OrgMode\enum\OrgWarningType.ps1"
+    . "$($Source.Path)\OrgMode\classes\OrgRepeater.ps1"
+    . "$($Source.Path)\OrgMode\classes\OrgWarning.ps1"
+    . "$($Source.Path)\OrgMode\classes\OrgTimeStamp.ps1"
+    . "$($Source.Path)\OrgMode\classes\OrgElement.ps1"
+    . "$($Source.Path)\OrgMode\classes\OrgHeadline.ps1"
+
+}
+BeforeAll {
+    . "$($Source.Path)\OrgMode\enum\OrgType.ps1"
+    . "$($Source.Path)\OrgMode\enum\OrgRepeatType.ps1"
+    . "$($Source.Path)\OrgMode\enum\OrgWarningType.ps1"
+    . "$($Source.Path)\OrgMode\classes\OrgRepeater.ps1"
+    . "$($Source.Path)\OrgMode\classes\OrgWarning.ps1"
+    . "$($Source.Path)\OrgMode\classes\OrgTimeStamp.ps1"
+    . "$($Source.Path)\OrgMode\classes\OrgElement.ps1"
+    . "$($Source.Path)\OrgMode\classes\OrgHeadline.ps1"
+}
+Describe "Testing OrgMode class OrgHeadline" -Tags @('unit', 'OrgHeadline') {
+    Context "Class can be loaded" {
+        It "Does not throw an error when instantiated" {
+            { $null = New-Object OrgHeadline } | Should -Not -Throw
+        }
+    }
+    Context "When a new object is created without arguments" {
+        BeforeAll {
+            $h = New-Object OrgHeadline
+        }
+
+        It "Should have a default Type of timestamp" {
+            $h.Type | Should -Be headline
+        }
+
+        It "Should have a level of 0" {
+            $h.Level | Should -Be 0
         }
     }
 }
--- tests/OrgMode/OrgProperty.Tests.ps1
+++ tests/OrgMode/OrgProperty.Tests.ps1
@@ -1,10 +1,76 @@
 
-InModuleScope -ModuleName "PSOrgMode" {
-    Describe "Testing OrgMode class OrgProperty" -Tags @('unit', 'OrgProperty') {
-    Context "Class can be loaded" {
-        It "Does not throw an error when instantiated" {
-            {$null = New-Object OrgProperty} | Should -Not -Throw
+BeforeDiscovery {
+    . "$($Source.Path)\OrgMode\Classes\OrgProperty.ps1"
+}
+BeforeAll {
+    . "$($Source.Path)\OrgMode\Classes\OrgProperty.ps1"
+}
+
+Describe "Testing OrgMode class OrgProperty" -Tags @('unit', 'OrgProperty') {
+    Context "When a new object is created" {
+        It "Should not throw an error" {
+            { $null = New-Object OrgProperty } | Should -Not -Throw
         }
     }
-}
+    Context "When a new object is created without arguments" {
+        BeforeAll {
+            $p = New-Object OrgProperty
+        }
+
+        It "Should have a blank Name" {
+            $p.Name | Should -BeNullOrEmpty
+        }
+        It "Should have a blank Value" {
+            $p.Value | Should -BeNullOrEmpty
+        }
+    }
+
+    Context "When a new object is created with one argument <PropName>" -Foreach @(
+        @{
+            PropName = 'Gardening'
+        }
+    ){
+        BeforeAll {
+            $p = New-Object OrgProperty -ArgumentList $PropName
+        }
+
+        It "Should set the Name to <PropName>" {
+            $p.Name | Should -Be $PropName
+        }
+    }
+    Context "When a new object is created with two arguments '<PropName>' '<PropValue>'" -Foreach @(
+        @{
+            PropName = 'CATEGORY'
+            PropValue = 'Gardening'
+        }
+    ){
+        BeforeAll {
+            $p = New-Object OrgProperty -ArgumentList $PropName, $PropValue
+        }
+
+        It "Should set the Name to <PropName>" {
+            $p.Name | Should -Be $PropName
+        }
+        It "Should set the Value to <PropValue>" {
+            $p.Value | Should -Be $PropValue
+        }
+    }
+
+    Context "When a new object is created with an array of values '<PropName>' '<PropValue>'" -Foreach @(
+        @{
+            PropName = 'Letters'
+            PropValue = @('G', 'A')
+        }
+    ){
+        BeforeAll {
+            $p = New-Object OrgProperty -ArgumentList $PropName, $PropValue
+        }
+
+        It "Should set the Name to <PropName>" {
+            $p.Name | Should -Be $PropName
+        }
+        It "Should set the Value to <PropValue>" {
+            $p.Value | Should -Be $PropValue
+        }
+    }
 }
