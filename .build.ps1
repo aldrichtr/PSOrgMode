7aa72b86574ab5a4ce0080c314913014d5b36045
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-03-14 18:20:44 -0400

    feat: Added a test tag parameter to the test runner

--- .build.ps1
+++ .build.ps1
@@ -10,10 +10,48 @@ param(
     # functions
     [Parameter()]
     [string]
-    $ModuleName = 'PSOrgMode'
+    $ModuleName = 'PSOrgMode',
+
+    # Tags to filter the Pester Tests
+    [Parameter(
+    )]
+    [string]$TestTags
 )
 
 
 . ./build/BuildTool.ps1
 
-task Test unit_test_source
+task Test {
+    $pConfig = New-PesterConfiguration
+    $pConfig.Run.Path = "$BuildRoot\tests"
+    $pConfig.Run.SkipRemainingOnFailure = 'Container'
+    $pConfig.Filter.ExcludeTag = @('analyze')
+    $pConfig.Output.Verbosity = 'Detailed'
+    if ($null -ne $TestTags) {
+        $pConfig.Filter.Tag = @($TestTags)
+    }
+    Invoke-Pester -Configuration $pConfig
+}
+
+task stage_orgmode_module {
+    foreach ($line in (Get-Content "$BuildRoot\source\LoadOrder.txt")) {
+        Write-Build Red "this line is $line"
+        switch -Regex ($line) {
+            '^\s*$' {
+                # blank line, skip
+                continue
+            }
+            '^\s*#.*$' {
+                # Comment line, skip
+                continue
+            }
+            '^(?<module>\w+)\\.*\.ps1$' {
+                $file = "$BuildRoot\source\$line"
+                Get-Content $file | Add-Content -Path "$BuildRoot\stage\PSOrgMode\$($Matches.module).psm1"
+            }
+            Default {
+                # nothing to do
+            }
+        }
+    }
+}
